6675561b488767323f79fd7f00922efe312f74c7
GamerVerse <cristian987654321rios@gmail.com>
2025-02-28 13:47:03 -0600

    added documentation to BAseButton.hpp

--- include/user/BaseButton.hpp
+++ include/user/BaseButton.hpp
@@ -6,7 +6,9 @@
 namespace button{
     class BaseButton {
         private:
+            /* The list of registerd buttons. */
             static std::vector<BaseButton*> buttonActions;
+            /* The button registerd to run a certain action. */
             pros::controller_digital_e_t button;
             bool wasPressed = false;
             bool enabledTask = false;
@@ -15,10 +17,15 @@ namespace button{
             BaseButton(pros::controller_digital_e_t button);
             virtual ~BaseButton() = default;
 
+            /* Runs all the button actions on it's own task. */
             static void run();
+            /* Deletes all registered buttons. */
             static void reset();
+            /* Runs the onPress and onRelease functions. */
             void update();
+            /* Runs button task on press the button registered. */
             virtual void onPress() {}
+            /* Runs button task on release the button registered. */
             virtual void onRelease() {}
     };
 
